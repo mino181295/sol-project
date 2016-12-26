4b9f1dfa307142ccd2d9a940fadcd636d7a3a59b
Paolo Venturi <paolo.venturi9@studio.unibo.it>
2016-12-26 20:16:53 +0100

    Calendar modified.

---
+++ app/Calendar/calendar.js
@@ -0,0 +1,33 @@
+$(function(){
+
+    var monthNames = ['Gennaio', 'Febbrario', 'Marzo','Aprile', 'Maggio', 'Giugno', 'Luglio',
+                       'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'];
+
+    var dayNames = ['Lunedì', 'Martedì', 'Mercoledì', 'Giovedì', 'Sabato', 'Domenica'];
+
+    function showDate() {
+        $(".month-yearInfo").text(monthNames[currentMonth] + " - " + currentYear);
+    }
+
+    function getDaysInMonth(year, month) {
+        return new Date(year, month+1, 0).getDate();
+    }
+
+    $("#nextMonth").click(function() {
+        currentMonth = (currentMonth+1)%12;
+        currentYear += currentMonth == 0 ? 1 : 0
+        showDate();
+    });
+
+    $("#prevMonth").click(function() {
+        currentMonth = (currentMonth-1+12)%12;
+        currentYear -= currentMonth == 11 ? 1 : 0
+        showDate();
+    });
+
+    /** MAIN **/
+    var d = new Date();
+    var currentYear = d.getFullYear();
+    var currentMonth = d.getMonth();
+    showDate();
+});
