644f191fbec7be74f73a87e885d3cf99028a52bc
Lorenzo Chiana <lorenzo.chiana@studio.unibo.it>
2017-01-10 20:39:31 +0100

    restyle with bootstrap

--- login/login.php
+++ login/login.php
@@ -6,46 +6,40 @@
 
 	<meta charset="utf-8">
 
-	<title>Login</title>
-
+	<title>Accedi</title>
+	<link href="http://netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
 	<link rel="stylesheet" href="css/style.css">
 
+
 </head>
-
 <body>
-
-	<div id="login">
-
-		<div id="header">
-			<figure>
-					<img src="img/logo.png" alt="">
-			</figure>
-		</div>
-
-		<form action="process_login.php" method="post" name="login_form">
-
-			<fieldset>
-
-				<label for="email">Indirizzo e-mail</label></br></br>
-				<input type="email" name="email" id="email" autocomplete="on" placeholder="es. mario.rossi@unibo.it, mario.rossi@studio.unibo.it" required /></br></br>
-
-				<label for="password">Password</label></br></br>
-				<input type="password" name="pw" id="password" placeholder="Password" required /></br></br>
-
-				<input type="submit" value="Accedi" onclick="formhash(this.form, this.form.password);" >
-
-			</fieldset>
-
-		</form>
-
+	<div class="container">
+	    <div class="row login">
+	        <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 well">
+	            <form role="form" action="process_login.php" method="post" name="login_form">
+	              <div class="form-group text-center">
+	                <div class="logo">
+	                    <span class="glyphicon glyphicon-user set-logo"></span>
+	                </div>
+	              </div>
+	              <div class="form-group">
+	                <input type="email" class="form-control input-lg" name="email" autocomplete="on" placeholder="E-mail">
+	              </div>
+	              <div class="form-group">
+	                <input type="password" class="form-control input-lg" name="pw" placeholder="Password">
+	              </div>
+	              <div class="form-group">
+	                <button type="submit" class="btn btn-default btn-lg btn-block btn-success" onclick="formhash(this.form, this.form.password);">Accedi</button>
+	              </div>
+	              <div class="form-group last-row">
+	                <label class="checklabel">
+	                    <input type="checkbox"> Ricordami
+	                </label>
+	                <a href="#" class="pull-right">Password dimenticata</a>
+	              </div>
+	            </form>
+	        </div>
+	    </div>
 	</div>
-
-	<div id="introduction" class="groupMargin">
-		<div id="brandname">
-			<p class="titoletto">Alma Mater Studiorum</p>
-			<p class="nome">Università di Bologna</p>
-		</div>
-	</div>
-
-</body>	
+</body>
 </html>
