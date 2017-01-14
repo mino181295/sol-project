2790b88369203f7dc8df33c3c2cd9acc58971350
Lorenzo Chiana <lorenzo.chiana@studio.unibo.it>
2017-01-14 13:29:45 +0100

    restyle + accessibility

--- login/login.php
+++ login/login.php
@@ -2,12 +2,13 @@
 <html>
 <head>
 	<script type="text/javascript" src="sha512.js"></script>
-	<script type="text/javascript" src="forms.js"></script>
+	<script type="text/javascript" src="forms.js"></script>		
 
 	<meta charset="utf-8">
 
 	<title>Accedi</title>
-	<link href="http://netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="../app/lib/Bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="../app/lib/font-awesome-4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="css/style.css">
 
 
@@ -27,31 +28,70 @@
 						   $error = $_GET['error'];
 							if($error == 1) {
 								//email sbagliata
-								echo 
-								'<div class="form-group has-error has-feedback">
-		                			<input type="email" class="form-control input-lg" name="email" autocomplete="on" placeholder="E-mail" />
-		              			</div>
-		              			<div class="form-group">
-		                			<input type="password" class="form-control input-lg" id="password" name="pw" placeholder="Password" />
-		              			</div>';
+								echo
+		              			'<div class="form-group">
+									<label for="email" class="cols-sm-2 control-label">Email</label>
+									<div class="cols-sm-10">
+										<div class="input-group has-error has-feedback">
+											<span class="input-group-addon"><i class="fa fa-envelope fa" aria-hidden="true"></i></span>
+											<input type="email" class="form-control input-lg" name="email" id="email" autocomplete="on" placeholder="es. mario.rossi@unibo.it, mario.rossi@studio.unibo.it"/>
+										</div>
+									</div>
+								</div>
+
+								<div class="form-group">
+									<label for="password" class="cols-sm-2 control-label">Password</label>
+									<div class="cols-sm-10">
+										<div class="input-group">
+											<span class="input-group-addon"><i class="fa fa-lock fa-lg" aria-hidden="true"></i></span>
+											<input type="password" class="form-control input-lg" name="password" id="password"  placeholder="Password"/>
+										</div>
+									</div>
+								</div>';
 							} else if($error == 2) {
 								//password sbagliata
 								echo 
 								'<div class="form-group">
-		                			<input type="email" class="form-control input-lg" name="email" autocomplete="on" placeholder="E-mail" />
-		              			</div>
-		              			<div class="form-group has-error has-feedback">
-		                			<input type="password" class="form-control input-lg" id="password" name="pw" placeholder="Password" />
-		              			</div>';
+									<label for="email" class="cols-sm-2 control-label">Email</label>
+									<div class="cols-sm-10">
+										<div class="input-group">
+											<span class="input-group-addon"><i class="fa fa-envelope fa" aria-hidden="true"></i></span>
+											<input type="text" class="form-control input-lg" name="email" id="email"  placeholder="es. mario.rossi@unibo.it, mario.rossi@studio.unibo.it"/>
+										</div>
+									</div>
+								</div>
+
+								<div class="form-group">
+									<label for="password" class="cols-sm-2 control-label">Password</label>
+									<div class="cols-sm-10">
+										<div class="input-group has-error has-feedback">
+											<span class="input-group-addon"><i class="fa fa-lock fa-lg" aria-hidden="true"></i></span>
+											<input type="password" class="form-control input-lg" name="password" id="password"  placeholder="Password"/>
+										</div>
+									</div>
+								</div>';
 							}
 						} else {
 							echo 
 							'<div class="form-group">
-	                			<input type="email" class="form-control input-lg" name="email" autocomplete="on" placeholder="E-mail" />
-	              			</div>
-	              			<div class="form-group">
-	                			<input type="password" class="form-control input-lg" id="password" name="pw" placeholder="Password" />
-	              			</div>';
+								<label for="email" class="cols-sm-2 control-label">Email</label>
+								<div class="cols-sm-10">
+									<div class="input-group">
+										<span class="input-group-addon"><i class="fa fa-envelope fa" aria-hidden="true"></i></span>
+										<input type="text" class="form-control input-lg" name="email" id="email"  placeholder="es. mario.rossi@unibo.it, mario.rossi@studio.unibo.it"/>
+									</div>
+								</div>
+							</div>
+
+							<div class="form-group">
+								<label for="password" class="cols-sm-2 control-label">Password</label>
+								<div class="cols-sm-10">
+									<div class="input-group">
+										<span class="input-group-addon"><i class="fa fa-lock fa-lg" aria-hidden="true"></i></span>
+										<input type="password" class="form-control input-lg" name="password" id="password"  placeholder="Password"/>
+									</div>
+								</div>
+							</div>';
 						}
 					?>
 					<!--<div class="form-group">
@@ -61,14 +101,27 @@
 	                			<input type="password" name="pw" id="password" class="form-control input-lg" placeholder="Password" required />
 	              			</div>-->
 
+	              		<!--<div class="form-group">
+							<label for="email" class="cols-sm-2 control-label">Email</label>
+							<div class="cols-sm-10">
+								<div class="input-group">
+									<span class="input-group-addon"><i class="fa fa-envelope fa" aria-hidden="true"></i></span>
+									<input type="text" class="form-control" name="email" id="email"  placeholder="es. mario.rossi@unibo.it, mario.rossi@studio.unibo.it"/>
+								</div>
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label for="password" class="cols-sm-2 control-label">Password</label>
+							<div class="cols-sm-10">
+								<div class="input-group">
+									<span class="input-group-addon"><i class="fa fa-lock fa-lg" aria-hidden="true"></i></span>
+									<input type="password" class="form-control" name="password" id="password"  placeholder="Password"/>
+								</div>
+							</div>
+						</div>-->
 	              <div class="form-group">
 	                <button type="submit" class="btn btn-default btn-lg btn-block btn-success" onclick="formhash(this.form, this.form.password);">Accedi</button>
-	              </div>
-	              <div class="form-group last-row">
-	                <label class="checklabel">
-	                    <input type="checkbox"> Ricordami
-	                </label>
-	                <a href="#" class="pull-right">Password dimenticata</a>
 	              </div>
 	            </form>
 	        </div>
