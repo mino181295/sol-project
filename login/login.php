9c71ea45c7a8fdc241b19680b2a5e1c8c365e2a1
Lorenzo Chiana <lorenzo.chiana@studio.unibo.it>
2017-01-17 20:00:22 +0100

    restyle + mobile first

--- login/login.php
+++ login/login.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" charset="utf-8"> 
 	<script type="text/javascript" src="sha512.js"></script>
 	<script type="text/javascript" src="forms.js"></script>		
-
-	<meta charset="utf-8">
 
 	<title>Accedi</title>
 	<link rel="stylesheet" href="../app/lib/Bootstrap/css/bootstrap.min.css">
@@ -19,6 +18,7 @@
 	        <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 well">
 	            <form role="form" action="process_login.php" method="post" name="login_form">
 	              <div class="form-group text-center">
+	              	<!-- LOGO UNIBO -->
 	                <div class="logo">
 	                    <!--<span class="glyphicon glyphicon-user set-logo"></span>-->
 	                    <img src="../app/image/logo_unibo.png" alt="">
@@ -28,7 +28,9 @@
 						if(isset($_GET['error'])) { 
 						   $error = $_GET['error'];
 							if($error == 1) {
-								//email sbagliata
+								/* EMAIL SBAGLIATA
+									Segnalato dal relativo campo input di colore rosso
+								*/
 								echo
 		              			'<div class="form-group">
 									<label for="email" class="cols-sm-2 control-label">E-mail</label>
@@ -37,6 +39,7 @@
 											<span class="input-group-addon"><i class="fa fa-envelope fa" aria-hidden="true"></i></span>
 											<input type="email" class="form-control input-lg" name="email" id="email" autocomplete="on" placeholder="es. mario.rossi@studio.unibo.it"/>
 										</div>
+										<p><b>E-mail non corretta!</b></p>
 									</div>
 								</div>
 
@@ -50,14 +53,16 @@
 									</div>
 								</div>';
 							} else if($error == 2) {
-								//password sbagliata
+								/* PASSWORD SBAGLIATA
+									Segnalato dal relativo campo input di colore rosso
+								*/
 								echo 
 								'<div class="form-group">
 									<label for="email" class="cols-sm-2 control-label">E-mail</label>
 									<div class="cols-sm-10">
 										<div class="input-group">
 											<span class="input-group-addon"><i class="fa fa-envelope fa" aria-hidden="true"></i></span>
-											<input type="text" class="form-control input-lg" name="email" id="email"  placeholder="es. mario.rossi@studio.unibo.it"/>
+											<input type="text" class="form-control input-lg" name="email" id="email"  placeholder="es. mario.rossi@studio.unibo.it" />
 										</div>
 									</div>
 								</div>
@@ -67,8 +72,9 @@
 									<div class="cols-sm-10">
 										<div class="input-group has-error has-feedback">
 											<span class="input-group-addon"><i class="fa fa-lock fa-lg" aria-hidden="true"></i></span>
-											<input type="password" class="form-control input-lg" name="password" id="password"  placeholder="Password"/>
+											<input type="password" class="form-control input-lg" name="password" id="password"  placeholder="Password" />
 										</div>
+										<p><b>Password non corretta!</b></p>
 									</div>
 								</div>';
 							}
@@ -79,7 +85,7 @@
 								<div class="cols-sm-10">
 									<div class="input-group">
 										<span class="input-group-addon"><i class="fa fa-envelope fa" aria-hidden="true"></i></span>
-										<input type="text" class="form-control input-lg" name="email" id="email"  placeholder="es. mario.rossi@studio.unibo.it"/>
+										<input type="text" class="form-control input-lg" name="email" id="email"  placeholder="es. mario.rossi@studio.unibo.it" />
 									</div>
 								</div>
 							</div>
@@ -89,38 +95,13 @@
 								<div class="cols-sm-10">
 									<div class="input-group">
 										<span class="input-group-addon"><i class="fa fa-lock fa-lg" aria-hidden="true"></i></span>
-										<input type="password" class="form-control input-lg" name="password" id="password"  placeholder="Password"/>
+										<input type="password" class="form-control input-lg" name="password" id="password"  placeholder="Password" />
 									</div>
 								</div>
 							</div>';
 						}
 					?>
-					<!--<div class="form-group">
-	                			<input type="email" class="form-control input-lg" name="email" autocomplete="on" placeholder="E-mail" required />
-	              			</div>
-	              			<div class="form-group">
-	                			<input type="password" name="pw" id="password" class="form-control input-lg" placeholder="Password" required />
-	              			</div>-->
 
-	              		<!--<div class="form-group">
-							<label for="email" class="cols-sm-2 control-label">Email</label>
-							<div class="cols-sm-10">
-								<div class="input-group">
-									<span class="input-group-addon"><i class="fa fa-envelope fa" aria-hidden="true"></i></span>
-									<input type="text" class="form-control" name="email" id="email"  placeholder="es. mario.rossi@unibo.it, mario.rossi@studio.unibo.it"/>
-								</div>
-							</div>
-						</div>
-
-						<div class="form-group">
-							<label for="password" class="cols-sm-2 control-label">Password</label>
-							<div class="cols-sm-10">
-								<div class="input-group">
-									<span class="input-group-addon"><i class="fa fa-lock fa-lg" aria-hidden="true"></i></span>
-									<input type="password" class="form-control" name="password" id="password"  placeholder="Password"/>
-								</div>
-							</div>
-						</div>-->
 	              <div class="form-group">
 	                <button type="submit" class="btn btn-default btn-lg btn-block btn-success" onclick="formhash(this.form, this.form.password);">Accedi</button>
 	              </div>
