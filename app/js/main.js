8f5f58f10caf81784612d9934438b9e0443b3c6d
Matteo Minardi <matteo.minardi@studio.unibo.it>
2017-01-22 14:21:33 +0100

    Implemented navbar and temp sidebar in home

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,32 @@
-console.log("Great");
-console.log(", it worked");
-console.log(", wow");
-console.log(", happy happy");
-console.log("LOL IT CHAnged");
-console.log("LOL IT Again");
+$(document).ready(function () {
+
+    var docHeight = $(document).height();
+    var docWidth = $(document).width();
+
+    if (docWidth < 800) {
+        $("#sol-label").text("SOL");
+        $("#school-information").hide();
+    } else {
+        $("#sol-label").text("Studenti Online");
+    }
+    $(window).resize(function () {
+        docHeight = $(document).height();
+        docWidth = $(document).width();
+
+        if (docWidth < 750) {
+            $("#sol-label").text("SOL");
+            $("#school-information").hide();
+
+        } else {
+            $("#sol-label").text("Studenti Online");
+            $("#school-information").show();
+        }
+
+    });
+
+    $("#menu-toggle").click(function (e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+
+});
