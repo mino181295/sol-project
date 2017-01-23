7e7cd70f3304dfe6067fb77c066676e0874dc100
Matteo Minardi <matteo.minardi@studio.unibo.it>
2017-01-23 18:37:33 +0100

    Home almost done

--- app/js/main.js
+++ app/js/main.js
@@ -3,18 +3,19 @@ $(document).ready(function () {
     var docHeight = $(document).height();
     var docWidth = $(document).width();
 
-    if (docWidth < 800) {
+    if (docWidth < 768) {
         $("#sol-label").text("SOL");
         $("#school-information").hide();
     } else {
         $("#sol-label").text("Studenti Online");
     }
-    
+
     $(window).resize(function () {
+
         docHeight = $(document).height();
         docWidth = $(document).width();
 
-        if (docWidth < 750) {
+        if (docWidth < 768) {
             $("#sol-label").text("SOL");
             $("#school-information").hide();
 
@@ -26,6 +27,7 @@ $(document).ready(function () {
     });
 
     $("#menu-toggle").click(function (e) {
+        alert("Tggoled");
         e.preventDefault();
         $("#wrapper").toggleClass("toggled");
     });
