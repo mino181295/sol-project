6d7c0c840208a7b97992927e237946a8ee7d9ec4
Matteo Minardi <matteo.minardi@studio.unibo.it>
2017-01-24 00:01:35 +0100

    Fixed sidebar and added some wow effects

--- app/js/main.js
+++ app/js/main.js
@@ -10,6 +10,15 @@ $(document).ready(function () {
         $("#sol-label").text("Studenti Online");
     }
 
+    if ($("#wrapper").hasClass("toggled")) {
+        $("#sidebar-btn").addClass("glyphicon-menu-right");
+        $("#sidebar-btn").removeClass("glyphicon-menu-left");
+    } else {
+
+        $("#sidebar-btn").addClass("glyphicon-menu-left");
+        $("#sidebar-btn").removeClass("glyphicon-menu-right");
+
+    }
     $(window).resize(function () {
 
         docHeight = $(document).height();
@@ -26,10 +35,47 @@ $(document).ready(function () {
 
     });
 
+    //Fade in dei bottoni
+    var eT = 0;
+    $('.btn-sq').hide().each(function () {
+        $(this).delay(eT).fadeIn('slow').addClass("animated bounceIn");
+
+        eT += 350;
+    });
+    $('.everything').click(function () {
+        $('.everything').stop(true, true).fadeIn();
+    });
+    //
+    $(".btn-sq *, .btn-sq").hover(function () {
+        $(this).closest(".btn-sq").css("background-color", "#660000");
+        $(this).closest(".btn-sq").children("i").addClass("animated pulse");
+
+    }, function () {
+        $(this).closest(".btn-sq").css("background-color", "#7f0000");
+        $(this).closest(".btn-sq").children("i").removeClass("animated pulse");
+
+    });
+    
+    $("#notification-button").hover(function () {
+        $("#notification-label").addClass("animated shake");
+    }, function () {
+        $("#notification-label").removeClass("animated shake");
+
+    });
+
     $("#menu-toggle").click(function (e) {
-        alert("Tggoled");
         e.preventDefault();
         $("#wrapper").toggleClass("toggled");
+
+        if ($("#wrapper").hasClass("toggled")) {
+            $("#sidebar-btn").addClass("glyphicon-menu-right");
+            $("#sidebar-btn").removeClass("glyphicon-menu-left");
+        } else {
+
+            $("#sidebar-btn").addClass("glyphicon-menu-left");
+            $("#sidebar-btn").removeClass("glyphicon-menu-right");
+
+        }
     });
 
 });
