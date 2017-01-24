1ebcd43d08308b0b0d3a79204abee24b99c2f91d
Matteo Minardi <matteo.minardi@studio.unibo.it>
2017-01-24 14:42:54 +0100

    Changed navbar and added effects

--- app/js/main.js
+++ app/js/main.js
@@ -2,27 +2,31 @@ $(document).ready(function () {
 
     var docHeight = $(document).height();
     var docWidth = $(document).width();
-
+    //Controllo per la label brand
     if (docWidth < 768) {
         $("#sol-label").text("SOL");
         $("#school-information").hide();
     } else {
         $("#sol-label").text("Studenti Online");
     }
-    
-    if (!$("#wrapper").hasClass("toggled")) {
-        $("#sidebar-btn").removeClass("glyphicon-menu-left");
-        $("#sidebar-btn").addClass("glyphicon-menu-right");
-    } else {
-        $("#sidebar-btn").removeClass("glyphicon-menu-right");
-        $("#sidebar-btn").addClass("glyphicon-menu-left");
+    //Controllo per il toggle
 
+    if (!$("#wrapper").hasClass("toggled") && docWidth > 768) {
+        $("#sidebar-btn").removeClass("fa-cogs");
+        $("#sidebar-btn").addClass("fa-arrow-left");
     }
-    
+    //Controllo nel resize se c'è da cambiare la label brand
     $(window).resize(function () {
-
         docHeight = $(document).height();
         docWidth = $(document).width();
+
+        if (!$("#wrapper").hasClass("toggled") && docWidth > 768) {
+            $("#sidebar-btn").removeClass("fa-cogs");
+            $("#sidebar-btn").addClass("fa-arrow-left");
+        } else {
+            $("#sidebar-btn").removeClass("fa-arrow-left");
+            $("#sidebar-btn").addClass("fa-cogs");
+        }
 
         if (docWidth < 768) {
             $("#sol-label").text("SOL");
@@ -37,15 +41,15 @@ $(document).ready(function () {
 
     //Fade in dei bottoni
     var eT = 0;
+    var randomEffectVector = ["flipInX", "flipInY"];
     $('.btn-sq').hide().each(function () {
-        $(this).delay(eT).fadeIn('fast').addClass("animated bounceIn");
-
+        $(this).delay(eT).fadeIn('fast').addClass("animated "+ randomEffectVector[Math.floor(Math.random()+1)]);
         eT += 350;
     });
-    $('.everything').click(function () {
-        $('.everything').stop(true, true).fadeIn();
+    $('.btn-sq').click(function () {
+        $('.btn-sq').stop(true, true).fadeIn();
     });
-    //
+    //Cambio background dei bottoni
     $(".btn-sq *, .btn-sq").hover(function () {
         $(this).closest(".btn-sq").css("background-color", "#660000");
         $(this).closest(".btn-sq").children("i").addClass("animated pulse");
@@ -55,27 +59,27 @@ $(document).ready(function () {
         $(this).closest(".btn-sq").children("i").removeClass("animated pulse");
 
     });
-    
+    //Shake delle notifiche
     $("#notification-button").hover(function () {
         $("#notification-label, #notification-number").addClass("animated shake");
     }, function () {
         $("#notification-label, #notification-number").removeClass("animated shake");
 
     });
-
+    //Cambio al click della sidebar
     $("#menu-toggle").click(function (e) {
         e.preventDefault();
         $("#wrapper").toggleClass("toggled");
 
-        if (!$("#wrapper").hasClass("toggled")) {
-            $("#sidebar-btn").removeClass("glyphicon-menu-left");
-            $("#sidebar-btn").addClass("glyphicon-menu-right");
-        } else {
+        $("#sidebar-btn").addClass("animated rotateIn");
+        setTimeout(function () {
+            $("#sidebar-btn").removeClass('animated rotateIn');
+        }, 1300);
 
-            $("#sidebar-btn").removeClass("glyphicon-menu-right");
-            $("#sidebar-btn").addClass("glyphicon-menu-left");
+        console.log("Fade out");
 
-        }
+        $("#sidebar-btn").toggleClass("fa-cogs fa-arrow-left");
+
     });
 
 });
