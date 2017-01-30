219b365db0eee837ad944ab4ae2aa0a939ee8cc3
Lorenzo Chiana <lorenzo.chiana@studio.unibo.it>
2017-01-30 21:56:28 +0100

    sql definitivo

--- db/myalma.sql
+++ db/myalma.sql
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `myalma`
@@ -29,12 +29,10 @@ SET time_zone = "+00:00";
 -- Struttura della tabella `assegnamento`
 --
 
-CREATE TABLE IF NOT EXISTS `assegnamento` (
+CREATE TABLE `assegnamento` (
   `CodiceCorso` char(5) NOT NULL,
   `IDCorsoStudi` char(5) NOT NULL,
-  `Docente` varchar(40) NOT NULL,
-  PRIMARY KEY (`Docente`,`CodiceCorso`,`IDCorsoStudi`),
-  KEY `FKCodiceCorso` (`CodiceCorso`,`IDCorsoStudi`)
+  `Docente` varchar(40) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
@@ -51,14 +49,12 @@ INSERT INTO `assegnamento` (`CodiceCorso`, `IDCorsoStudi`, `Docente`) VALUES
 -- Struttura della tabella `corso`
 --
 
-CREATE TABLE IF NOT EXISTS `corso` (
+CREATE TABLE `corso` (
   `Codice` char(5) NOT NULL,
   `IDCorsoStudi` char(5) NOT NULL,
   `Denominazione` varchar(40) NOT NULL,
   `Anno` int(1) NOT NULL,
-  `Ciclo` int(1) NOT NULL,
-  PRIMARY KEY (`Codice`,`IDCorsoStudi`),
-  KEY `FKofferta` (`IDCorsoStudi`)
+  `Ciclo` int(1) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
@@ -100,11 +96,10 @@ INSERT INTO `corso` (`Codice`, `IDCorsoStudi`, `Denominazione`, `Anno`, `Ciclo`)
 -- Struttura della tabella `corsostudi`
 --
 
-CREATE TABLE IF NOT EXISTS `corsostudi` (
+CREATE TABLE `corsostudi` (
   `ID` char(5) NOT NULL,
   `Denominazione` varchar(40) NOT NULL,
-  `DurataAnni` int(1) NOT NULL,
-  PRIMARY KEY (`ID`)
+  `DurataAnni` int(1) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
@@ -122,14 +117,13 @@ INSERT INTO `corsostudi` (`ID`, `Denominazione`, `DurataAnni`) VALUES
 -- Struttura della tabella `evento`
 --
 
-CREATE TABLE IF NOT EXISTS `evento` (
+CREATE TABLE `evento` (
   `Utente` varchar(40) NOT NULL,
-  `Numero` int(4) NOT NULL AUTO_INCREMENT,
+  `Numero` int(4) NOT NULL,
   `Inizio` datetime NOT NULL,
   `Fine` datetime NOT NULL,
-  `Descrizione` varchar(200) NOT NULL,
-  PRIMARY KEY (`Utente`,`Numero`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `Descrizione` varchar(200) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -137,12 +131,10 @@ CREATE TABLE IF NOT EXISTS `evento` (
 -- Struttura della tabella `iscrizione`
 --
 
-CREATE TABLE IF NOT EXISTS `iscrizione` (
+CREATE TABLE `iscrizione` (
   `IDCorsoStudi` char(5) NOT NULL,
   `Studente` varchar(40) NOT NULL,
-  `AnnoAccademico` varchar(9) NOT NULL,
-  PRIMARY KEY (`Studente`,`AnnoAccademico`),
-  KEY `FKUtente` (`Studente`)
+  `AnnoAccademico` varchar(9) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
@@ -161,16 +153,14 @@ INSERT INTO `iscrizione` (`IDCorsoStudi`, `Studente`, `AnnoAccademico`) VALUES
 -- Struttura della tabella `lezione`
 --
 
-CREATE TABLE IF NOT EXISTS `lezione` (
-  `Numero` int(3) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `lezione` (
+  `Numero` int(3) NOT NULL,
   `OraInizio` datetime NOT NULL,
   `OraFine` datetime NOT NULL,
   `Aula` varchar(40) NOT NULL,
   `CodiceCorso` char(5) NOT NULL,
-  `IDCorsoStudi` char(5) NOT NULL,
-  PRIMARY KEY (`Numero`,`CodiceCorso`,`IDCorsoStudi`),
-  KEY `FKesecuzione` (`CodiceCorso`,`IDCorsoStudi`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+  `IDCorsoStudi` char(5) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dump dei dati per la tabella `lezione`
@@ -186,27 +176,26 @@ INSERT INTO `lezione` (`Numero`, `OraInizio`, `OraFine`, `Aula`, `CodiceCorso`, 
 -- Struttura della tabella `notifica`
 --
 
-CREATE TABLE IF NOT EXISTS `notifica` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `notifica` (
+  `ID` int(11) NOT NULL,
   `Matricola_mit` char(10) NOT NULL,
   `Matricola_dest` char(10) NOT NULL,
   `Testo` varchar(255) NOT NULL,
   `Orario` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `Stato` char(1) NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+  `Stato` char(1) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dump dei dati per la tabella `notifica`
 --
 
 INSERT INTO `notifica` (`ID`, `Matricola_mit`, `Matricola_dest`, `Testo`, `Orario`, `Stato`) VALUES
-(1, '2', '1', 'Ha caricato nuovo materiale', '2016-10-05 09:24:39', '0'),
-(2, '2', '1', 'Ha modificato materiale', '2016-10-25 08:16:37', '0'),
-(3, '2', '1', 'Ha caricato nuovo materiale', '2016-11-09 13:53:25', '0'),
-(4, '2', '1', 'Ha annullato la lezione', '2017-01-29 17:10:57', '0'),
-(5, '2', '1', 'Ha fissato orario esame', '2017-01-29 17:10:57', '0'),
-(6, '2', '1', 'Ha verbaliazzato il voto d''esame', '2017-01-29 17:10:57', '0');
+(1, '3', '1', 'Ha caricato nuovo materiale', '2016-10-09 05:49:25', '0'),
+(2, '2', '1', 'Ha modificato materiale', '2016-10-28 16:33:26', '0'),
+(3, '5', '1', 'Ha caricato nuovo materiale', '2016-11-09 06:21:45', '1'),
+(4, '2', '1', 'Ha annullato la lezione', '2016-12-06 09:51:07', '1'),
+(5, '4', '1', 'Ha fissato orario esame', '2017-01-10 09:25:28', '1'),
+(6, '2', '1', 'Ha verbaliazzato il voto d\'esame', '2017-01-17 14:51:28', '1');
 
 -- --------------------------------------------------------
 
@@ -214,7 +203,7 @@ INSERT INTO `notifica` (`ID`, `Matricola_mit`, `Matricola_dest`, `Testo`, `Orari
 -- Struttura della tabella `utente`
 --
 
-CREATE TABLE IF NOT EXISTS `utente` (
+CREATE TABLE `utente` (
   `Email` varchar(40) NOT NULL,
   `TipoUtente` char(1) NOT NULL,
   `Nome` varchar(40) NOT NULL,
@@ -222,9 +211,7 @@ CREATE TABLE IF NOT EXISTS `utente` (
   `Password` varchar(128) NOT NULL,
   `ImmagineProfilo` varchar(255) NOT NULL,
   `Salt` char(128) NOT NULL,
-  `Matricola` char(10) DEFAULT NULL,
-  PRIMARY KEY (`Email`),
-  UNIQUE KEY `IDUtente_1` (`Matricola`)
+  `Matricola` char(10) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
@@ -236,8 +223,84 @@ INSERT INTO `utente` (`Email`, `TipoUtente`, `Nome`, `Cognome`, `Password`, `Imm
 ('franco.callegati@unibo.it', 'd', 'Franco', 'Callegati', 'de2c77702e05736c21f030100b2a008c050846e93bb02e27d7ff1edc61a6863c22f7dc9d4f45af2316ed7369a7f547e9d564ae96b17d3460ec0c928b5273e233', 'franco_callegati.jpg', '2206e453020e2597d8ae5ec7df893d0bdcd0bb25423d03d808dcb8adac4d49e9ec4a3c7b0800d71da7f9259293d98a210d6bcefbd6c2a5cd07a6779fff9ae182', '2'),
 ('paolo.venturi9@studio.unibo.it', 's', 'Paolo', 'Venturi', 'de2c77702e05736c21f030100b2a008c050846e93bb02e27d7ff1edc61a6863c22f7dc9d4f45af2316ed7369a7f547e9d564ae96b17d3460ec0c928b5273e233', 'paolo_venturi.jpg', '2206e453020e2597d8ae5ec7df893d0bdcd0bb25423d03d808dcb8adac4d49e9ec4a3c7b0800d71da7f9259293d98a210d6bcefbd6c2a5cd07a6779fff9ae182', '3'),
 ('rizzi.stefano@unibo.it', 'd', 'Stefano', 'Rizzi', 'de2c77702e05736c21f030100b2a008c050846e93bb02e27d7ff1edc61a6863c22f7dc9d4f45af2316ed7369a7f547e9d564ae96b17d3460ec0c928b5273e233', '', '2206e453020e2597d8ae5ec7df893d0bdcd0bb25423d03d808dcb8adac4d49e9ec4a3c7b0800d71da7f9259293d98a210d6bcefbd6c2a5cd07a6779fff9ae182', '4'),
-('luca.benini@studio.unibo.it', 's', 'Luca', 'Benini', 'de2c77702e05736c21f030100b2a008c050846e93bb02e27d7ff1edc61a6863c22f7dc9d4f45af2316ed7369a7f547e9d564ae96b17d3460ec0c928b5273e233', '', '2206e453020e2597d8ae5ec7df893d0bdcd0bb25423d03d808dcb8adac4d49e9ec4a3c7b0800d71da7f9259293d98a210d6bcefbd6c2a5cd07a6779fff9ae182', '6');
+('luca.benini@studio.unibo.it', 's', 'Luca', 'Benini', 'de2c77702e05736c21f030100b2a008c050846e93bb02e27d7ff1edc61a6863c22f7dc9d4f45af2316ed7369a7f547e9d564ae96b17d3460ec0c928b5273e233', '', '2206e453020e2597d8ae5ec7df893d0bdcd0bb25423d03d808dcb8adac4d49e9ec4a3c7b0800d71da7f9259293d98a210d6bcefbd6c2a5cd07a6779fff9ae182', '5');
 
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `assegnamento`
+--
+ALTER TABLE `assegnamento`
+  ADD PRIMARY KEY (`Docente`,`CodiceCorso`,`IDCorsoStudi`),
+  ADD KEY `FKCodiceCorso` (`CodiceCorso`,`IDCorsoStudi`);
+
+--
+-- Indici per le tabelle `corso`
+--
+ALTER TABLE `corso`
+  ADD PRIMARY KEY (`Codice`,`IDCorsoStudi`),
+  ADD KEY `FKofferta` (`IDCorsoStudi`);
+
+--
+-- Indici per le tabelle `corsostudi`
+--
+ALTER TABLE `corsostudi`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indici per le tabelle `evento`
+--
+ALTER TABLE `evento`
+  ADD PRIMARY KEY (`Utente`,`Numero`);
+
+--
+-- Indici per le tabelle `iscrizione`
+--
+ALTER TABLE `iscrizione`
+  ADD PRIMARY KEY (`Studente`,`AnnoAccademico`),
+  ADD KEY `FKUtente` (`Studente`);
+
+--
+-- Indici per le tabelle `lezione`
+--
+ALTER TABLE `lezione`
+  ADD PRIMARY KEY (`Numero`,`CodiceCorso`,`IDCorsoStudi`),
+  ADD KEY `FKesecuzione` (`CodiceCorso`,`IDCorsoStudi`);
+
+--
+-- Indici per le tabelle `notifica`
+--
+ALTER TABLE `notifica`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indici per le tabelle `utente`
+--
+ALTER TABLE `utente`
+  ADD PRIMARY KEY (`Email`),
+  ADD UNIQUE KEY `IDUtente_1` (`Matricola`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `evento`
+--
+ALTER TABLE `evento`
+  MODIFY `Numero` int(4) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT per la tabella `lezione`
+--
+ALTER TABLE `lezione`
+  MODIFY `Numero` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT per la tabella `notifica`
+--
+ALTER TABLE `notifica`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
