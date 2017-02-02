8f8fcb13086f99cc17d2461b4c538c34fd33dcc0
Matteo Minardi <matteo.minardi@studio.unibo.it>
2017-02-02 12:00:09 +0100

    Inserita prima versione delle notifiche al caricamento

---
+++ app/php/insert_notification.php
@@ -0,0 +1,47 @@
+<?php
+    include '../../login/functions.php';
+    include '../../login/db_connect.php';
+
+    sec_session_start();
+
+    $stmt = $mysqli->stmt_init();
+
+    $sql_idcorso = "SELECT IDCorsoStudi
+                  FROM utente u, corsostudi c, iscrizione i
+                  WHERE matricola = ? AND email = Studente AND ID = IDCorsoStudi  
+                  LIMIT 1;";
+
+    $stmt = $mysqli->prepare($sql_idcorso);
+    $stmt->bind_param("i", $_SESSION['matricola']);
+    $stmt->execute();
+    $stmt->store_result();
+    $stmt->bind_result($risultato_corso);  
+
+    if ($stmt->num_rows==1){
+        while ($stmt->fetch()){
+            $id_corso = $risultato_corso;
+        }
+    }
+
+    $sql_inserimento = "INSERT INTO notifica(Matricola_mit, Matricola_dest, testo, Stato)
+    VALUES (".$_SESSION['matricola'].", ?, 'Materiale caricato nel servizio studio', 1);";
+
+    $stmt = $mysqli->prepare($sql_inserimento);
+   
+    $sql_utenti = "SELECT matricola 
+                   FROM utente u, iscrizione i 
+                   WHERE matricola <> ".$_SESSION['matricola']." 
+                   AND email = Studente 
+                   AND IDCorsoStudi = ".$id_corso.";";
+
+	$result = $mysqli->query($sql_utenti);
+
+    if ($result->num_rows > 0) {
+	    while ($row = $result->fetch_assoc()) {
+            $matricola_destinatario = $row['matricola'];
+            $stmt->bind_param("i", $matricola_destinatario);
+            $stmt->execute();
+        }	    
+	}   
+	$stmt->close();
+?>
--- app/snippets/materia-snippet.php
+++ app/snippets/materia-snippet.php
@@ -9,7 +9,7 @@
     $files = array_diff(scandir($files_directory), array('.', '..'));
 
 ?>
-    
+
     <div class="row">
         <div class="col-xs-1 col-md-1 back-to-service">
             <button type="button" class="btn btn-labeled" id="back-to-service"> <span class="btn-label"><i class="glyphicon glyphicon-arrow-left"></i> </span> </button>
@@ -21,55 +21,59 @@
 
     <div class="row text-center">
 
-    <div class="container-fluid">
-        <br/>
-        <div class="row">
-            <div class="col-md-7">
-                <div class="panel panel-default">
-                    <div class="panel-heading"><strong>Upload files</strong> <small> </small></div>
-                    <div class="panel-body">
-                        <!-- Drop Zone -->
-                        <script type="text/javascript">
-                            $('#drop-zone').dropzone({
-                                addRemoveLinks: true,
-                                paramName: "file",
-                                dictDefaultMessage: "Trascina o clicca per caricare",
-                                maxFilesize: 10000,
-                                maxThumbnailFilesize: 3,
-                                init: function () {
-                                    this.on('complete', function (file) {});
-                                    this.on('success', function (file, json) {});
-                                    this.on('addedfile', function (file) {});
-                                    this.on("dragenter", function () {
-                                        $('#drop-zone').toggleClass(".drag-over");
-                                    });
-                                    this.on("dragleave", function () {
-                                        $('#drop-zone').toggleClass(".drag-over");
-                                    });
-                                }
-                            });
-                        </script>
+        <div class="container-fluid">
+            <br/>
+            <div class="row">
+                <div class="col-md-7">
+                    <div class="panel panel-default">
+                        <div class="panel-heading"><strong>Upload files</strong> <small> </small></div>
+                        <div class="panel-body">
+                            <!-- Drop Zone -->
+                            <script type="text/javascript">
+                                $('#drop-zone').dropzone({
+                                    addRemoveLinks: true,
+                                    paramName: "file",
+                                    dictDefaultMessage: "Trascina o clicca per caricare",
+                                    maxFilesize: 10000,
+                                    maxThumbnailFilesize: 3,
+                                    init: function () {
+                                        this.on('complete', function (file) {});
+                                        this.on('success', function (file, json) {
+                                            $.ajax({
+                                                type: "POST",
+                                                url: "php/insert_notification.php",
+                                                async: false,
+                                                success: function (text) {
+                                                }
+                                            });
+                                        });
+                                        this.on('addedfile', function (file) {});
+                                        this.on("dragenter", function () {});
+                                        this.on("dragleave", function () {});
+                                    }
+                                });
+                            </script>
 
-                        <form action="php/upload.php?path=<?php echo $materia;?>" class="dropzone dz-clickable upload-drop-zone" drop-zone="" id="drop-zone">
-                        </form>
-                        <!-- Upload Finished -->
+                            <form action="php/upload.php?path=<?php echo $materia;?>" class="dropzone dz-clickable upload-drop-zone" drop-zone="" id="drop-zone">
+                            </form>
+                            <!-- Upload Finished -->
 
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-5">
-                <div class="panel panel-default">
-                    <div class="panel-heading"><strong>Download files</strong> <small> </small></div>
-                    <div class="panel-body">
-                        <table class="table table-hover">
-                            <thead>
-                                <tr>
-                                    <th data-sortable="true" class="column-01">File </th>
-                                    <th data-sortable="true" class="column-02">Download</th>
-                                </tr>
-                            </thead>
-                            <tbody id="downloads-panel">
-                                <?php
+                <div class="col-md-5">
+                    <div class="panel panel-default">
+                        <div class="panel-heading"><strong>Download files</strong> <small> </small></div>
+                        <div class="panel-body">
+                            <table class="table table-hover">
+                                <thead>
+                                    <tr>
+                                        <th data-sortable="true" class="column-01">File </th>
+                                        <th data-sortable="true" class="column-02">Download</th>
+                                    </tr>
+                                </thead>
+                                <tbody id="downloads-panel">
+                                    <?php
                                         foreach($files as $name){
                                            echo '<tr>'; 
                                            echo '<td class="column-01"><a data-toggle="tooltip" title="'.$name.'" href="#">'.substr($name, 0, strpos($name, ".")).'</a></td>';
@@ -81,12 +85,12 @@
                                            echo '</tr>'; 
                                         }
                                     ?>
-                            </tbody>
-                        </table>
-                        <br/>
+                                </tbody>
+                            </table>
+                            <br/>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
